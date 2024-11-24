aaeba41df79388ec5ed68dfd381315ad9845717e
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-11-24 15:49:46 +0900

    Update camera.py

--- rpi/camera.py
+++ rpi/camera.py
@@ -1,21 +1,29 @@
 from fastapi import FastAPI, Response
-import cv2
 import asyncio
+from picamera2 import Picamera2
+import cv2
+import numpy as np
 
 app = FastAPI()
 
-# 카메라 초기화
-camera = cv2.VideoCapture(0)  # 0번 카메라 (기본 웹캠 사용)
+# Picamera2 객체 초기화
+picam2 = Picamera2()
+
+# 카메라 설정
+camera_config = picam2.create_preview_configuration(main={"size": (640, 480)})
+picam2.configure(camera_config)
+picam2.start()
 
 # 영상 데이터를 생성하는 함수
 async def generate_video_frames():
     while True:
-        success, frame = camera.read()
-            if not success:
-            break
+        # 프레임 캡처
+        frame = picam2.capture_array()  # Capture frame as a numpy array
+
         # 프레임을 JPEG 형식으로 인코딩
         _, buffer = cv2.imencode('.jpg', frame)
         frame_bytes = buffer.tobytes()
+
         # 스트림으로 프레임 반환
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
