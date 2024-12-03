c74ca7f7694321a29bff7ca3b79ed54630a6d72e
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-12-03 17:17:35 +0900

    Update silva.py

--- rpi/silva.py
+++ rpi/silva.py
@@ -1,22 +1,10 @@
-from edge_tpu_silva import process_segmentation
-from picamera2 import Picamera2
-import numpy as np
+from edge_tpu_silva import process_detection
 
-# 모델 파일 및 설정
-model_path = '192_yolov8n_full_integer_quant_edgetpu.tflite'
-imgsz = 192
+# Run the object detection process
+outs = process_detection(model_path='192_yolov8n_full_integer_quant_edgetpu.tflite', input_path='videoplayback.mp4', imgsz=192)
 
-# 동영상 스트리밍 처리
-try:
-    while True:
-        # 카메라에서 프레임 캡처
-        # 객체 분할 수행
-        # input_path에 "Camera(0)"을 전달하여 카메라로부터 직접 입력을 받음
-        outs = process_segmentation(model_path=model_path, input_path="Camera(0)", imgsz=imgsz, show=True)
-
-        # outs를 활용한 추가 처리가 필요하면 여기에서 진행
-        # 예: 분할된 객체의 정보 활용 등
-finally:
-    exit()
-    # 카메라 종료 및 자원 해제
-
+for objs_lst, fps in outs:
+    # Access the output parameters as needed
+    print(f"Processed frame with {len(objs_lst)} objects. FPS: {fps}")
+    print("List of object predictions in frame:")
+    print(objs_lst)
