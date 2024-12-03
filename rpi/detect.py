effa4a0aaf1129e4a96c75553de71a168971c6a9
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-12-03 17:34:48 +0900

    Update detect.py

--- rpi/detect.py
+++ rpi/detect.py
@@ -1,7 +1,7 @@
 from edge_tpu_silva import process_detection
 
 # Run the object detection process
-outs = process_detection(model_path='192_yolov8n_full_integer_quant_edgetpu.tflite', input_path='0', imgsz=192)
+outs = process_detection(model_path='192_yolov8n_full_integer_quant_edgetpu.tflite', input_path='0', imgsz=192, show=True)
 
 for _, _ in outs:
   pass
