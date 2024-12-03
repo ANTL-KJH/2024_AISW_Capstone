8f042336f8cbc27ba1c8b8c17ba220ddfb103296
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-12-03 17:02:07 +0900

    Update detect2.py

--- rpi/detect2.py
+++ rpi/detect2.py
@@ -1,36 +1,29 @@
-from edge_tpu_silva import process_detection
-import picamera
+import libcamera
 import cv2
 import numpy as np
-import time
 
 # 모델 파일 및 설정
 model_path = '192_yolov8n_full_integer_quant_edgetpu.tflite'
 imgsz = 192
 
-# Picamera 초기화
-camera = picamera.PiCamera()
-camera.resolution = (imgsz, imgsz)
-camera.framerate = 30
-
-# 카메라 프레임 저장을 위한 버퍼 초기화
-frame_buffer = np.empty((imgsz, imgsz, 3), dtype=np.uint8)
+# 카메라 초기화
+camera = libcamera.Camera()
+camera.configure()
 
 try:
-    print("Press 'ESC' in the OpenCV window to exit.")
-
+    print("Press 'ESC' to exit.")
     while True:
         # 카메라에서 프레임 캡처
-        camera.capture(frame_buffer, format='rgb', use_video_port=True)
+        frame = camera.capture()
 
         # 모델 입력 형식으로 전처리
-        input_data = np.expand_dims(frame_buffer, axis=0)  # 배치 차원 추가
+        input_data = np.expand_dims(frame, axis=0)  # 배치 차원 추가
 
         # 객체 탐지 수행
         outs = process_detection(model_path, input_data, imgsz)
 
         # 탐지 결과 시각화
-        frame_bgr = cv2.cvtColor(frame_buffer, cv2.COLOR_RGB2BGR)  # OpenCV는 BGR 형식 사용
+        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # OpenCV는 BGR 형식 사용
         for result in outs:
             box, cls, score = result["box"], result["class"], result["score"]
 
